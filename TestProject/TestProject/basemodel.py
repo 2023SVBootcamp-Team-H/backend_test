7076320d77f583cf4cece4dc29d731bbce80d865
heesane <tees3359@gmail.com>
2023-07-11 00:50:20 +0900

    !HOTFIX

--- TestProject/TestProject/basemodel.py
+++ TestProject/TestProject/basemodel.py
@@ -1,9 +1,10 @@
 from django.db import models
 
+
 class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    deleted_at = models.DateTimeField(blank=True,null=True)
-    
+    deleted_at = models.DateTimeField(blank=True, null=True)
+
     class Meta:
         abstract = True
