dd38af38d4a1a85df99fca03e7c954a79a4c450e
heesane <tees3359@gmail.com>
2023-07-10 20:35:04 +0900

    feat : BaseModel

--- TestProject/worry/models.py
+++ TestProject/worry/models.py
@@ -1,20 +1,17 @@
 from django.db import models
-
+from TestProject.basemodel import BaseModel
 # Create your models here.
-class Worry(models.Model): # Worry 모델
+class Worry(BaseModel): # Worry 모델
     # TODO: 유저 모델을 연결해야함(FK)
     # TODO: 카테고리 모델을 연결해야함(FK)
     # TODO: 인격 모델을 연결해야함(FK)
     # TODO: 답변 모델을 연결해야함(FK)
     content = models.TextField() # 내용
-    created_at = models.DateTimeField(auto_now_add=True) # 생성일
-    modified_at = models.DateTimeField(auto_now=True) # 수정일
-    deleted_at = models.DateTimeField(null=True, blank=True) # 삭제일
     
     def __str__(self):
         return self.content
 
-class Answer(models.Model): # Answer 모델
+class Answer(BaseModel): # Answer 모델
     worry = models.ForeignKey(Worry, on_delete=models.CASCADE) # Worry 모델과 1:1 관계
     content = models.CharField(max_length=300) # GPT가 답변한 내용
     
