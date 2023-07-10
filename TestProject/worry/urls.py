fc045f1f51114ce12460c00802ad340907fa385b
heesane <tees3359@gmail.com>
2023-07-10 23:14:23 +0900

    api test

--- TestProject/worry/urls.py
+++ TestProject/worry/urls.py
@@ -1,17 +1,10 @@
 from django.urls import path, include
 from django.contrib import admin
 from rest_framework import routers
-
-from worry.views import WorryViewSet, AnswerViewSet
+from .views import get_worry
 
 router = routers.DefaultRouter()
 
-router.register('worry', WorryViewSet)
-router.register('answer', AnswerViewSet)
-
-app_name = "worry app"
-
 urlpatterns = [
-    path('admin/', admin.site.urls, name="admin"),
-    path('', include(router.urls)),
+    path('<int:worry_id>', get_worry),
 ]
--- TestProject/worry/views.py
+++ TestProject/worry/views.py
@@ -1,13 +1,23 @@
 from rest_framework import viewsets
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
+from rest_framework.request import Request
 from .models import Worry, Answer
-from .serializer import WorrySerializer, AnswerSerializer
+
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Worry
 
 
 @api_view(['GET'])
-def get_worry(request):
-    worries = Worry.objects.all()
-    serializer = WorrySerializer(worries, many=True)
-    return Response(serializer.data)
+def get_worry(request, worry_id):
+    try:
+        worry = Worry.objects.get(id=worry_id)
+        content = {
+            'id': worry.id,
+            'content': worry.content
+        }
+        return Response(content)
+    except Worry.DoesNotExist:
+        return Response(status=404)
