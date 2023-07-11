ddb4f4dcd419626107047790f8e3788adb723914
이원찬 <twoone14@tukorea.ac.kr>
2023-07-11 23:20:31 +0900

    fix : personality api url 수정

--- TestProject/personality/urls.py
+++ TestProject/personality/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.personalities),
     path('score/', views.personalities_score),
-    path('create/', views.create_personality),
-    path('delete/', views.delete_personality),
+    # path('create/', views.create_personality),
+    # path('delete/', views.delete_personality),
 ]
--- TestProject/personality/views.py
+++ TestProject/personality/views.py
@@ -5,13 +5,6 @@ from .models import Personality
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
 
-
-@api_view(['GET'])
-def personalities(request):
-    personalities = Personality.objects.values()  # 쿼리셋(QuerySet)을 딕셔너리 형태로 반환
-    return Response(personalities)
-
-
 score_request_body_schema = openapi.Schema(
     type=openapi.TYPE_OBJECT,
     properties={
@@ -27,6 +20,53 @@ score_response_schema = openapi.Schema(
         # 필요한 필드들을 추가로 정의합니다.
     }
 )
+
+init_request_body_schema = openapi.Schema(
+    type=openapi.TYPE_OBJECT,
+    properties={
+        'personality_name': openapi.Schema(type=openapi.TYPE_STRING),
+    }
+)
+init_response_schema = openapi.Schema(
+    type=openapi.TYPE_OBJECT,
+    properties={
+        'message': openapi.Schema(type=openapi.TYPE_STRING),
+    }
+)
+
+
+@swagger_auto_schema(
+    method='get',
+    operation_description="인격 전체 조회",
+)
+@swagger_auto_schema(
+    method='post',
+    request_body=init_request_body_schema,
+    responses={200: init_response_schema},
+    operation_description="post.body.personality_name에 해당하는 인격 생성"
+)
+@swagger_auto_schema(
+    method='delete',
+    operation_description="인격 모두 삭제"
+)
+@api_view(['GET', 'POST', 'DELETE'])
+def personalities(request: Request):
+    if request.method == 'GET':
+        personalities = Personality.objects.values()  # 쿼리셋(QuerySet)을 딕셔너리 형태로 반환
+        return Response(personalities)
+    elif request.method == 'POST':
+        try:
+            name = request.data.get("personality_name")
+        except Exception as e:
+            return Response({"message": "no attribute personality_name"})
+        Personality(name=name, frequency=0, popularity=0, total=0).save()
+
+        return Response({"message": "Personality objects created successfully"})
+    elif request.method == 'DELETE':
+        p_list = Personality.objects.all()
+        for i in p_list:
+            i.delete()
+        return Response({"message": "Personality objects all Delete"})
 
 
 @swagger_auto_schema(method='post', request_body=score_request_body_schema, responses={200: score_response_schema})
@@ -61,20 +101,6 @@ def personalities_score(request: Request):
     return Response({"message": f"{personality_name}인격의 인기도가 {score}만큼 올라갔습니다."})
 
 
-init_request_body_schema = openapi.Schema(
-    type=openapi.TYPE_OBJECT,
-    properties={
-        'personality_name': openapi.Schema(type=openapi.TYPE_STRING),
-    }
-)
-init_response_schema = openapi.Schema(
-    type=openapi.TYPE_OBJECT,
-    properties={
-        'message': openapi.Schema(type=openapi.TYPE_STRING),
-    }
-)
-
-
 @swagger_auto_schema(method='post', request_body=init_request_body_schema, responses={200: init_response_schema})
 @api_view(['POST'])
 def create_personality(request: Request):
@@ -82,7 +108,7 @@ def create_personality(request: Request):
         name = request.data.get("personality_name")
     except Exception as e:
         return Response({"message": "no attribute personality_name"})
-    Personality(name=name, frequency=0, popularity=0, total = 0).save()
+    Personality(name=name, frequency=0, popularity=0, total=0).save()
 
     return Response({"message": "Personality objects created successfully"})
 
