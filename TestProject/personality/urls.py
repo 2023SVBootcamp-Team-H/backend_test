46399f29e4710b4e0d387725bc3eb20badf0785f
이원찬 <twoone14@tukorea.ac.kr>
2023-07-11 22:13:43 +0900

    feat : 인격 테스트 api 추가

--- TestProject/personality/urls.py
+++ TestProject/personality/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.personalities),
     path('score/', views.personalities_score),
-    path('init/', views.init_personality),
+    path('create/', views.create_personality),
+    path('delete/', views.delete_personality),
 ]
