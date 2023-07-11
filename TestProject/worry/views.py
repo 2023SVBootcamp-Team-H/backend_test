eed6cbef9eb54ee0aefdc0f26697fa9760ebd7bf
heesane <tees3359@gmail.com>
2023-07-11 16:11:17 +0900

    feat : worry post

--- TestProject/worry/views.py
+++ TestProject/worry/views.py
@@ -3,8 +3,11 @@ from rest_framework.response import Response
 from rest_framework.request import Request
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
-from .models import Worry
 
+from .models import Worry
+from user.models import User
+from category.models import Category
+from personality.models import Personality
 
 worry_request_body_schema = openapi.Schema(
     type=openapi.TYPE_OBJECT,
@@ -14,8 +17,9 @@ worry_request_body_schema = openapi.Schema(
         "job": openapi.Schema(type=openapi.TYPE_STRING),
         "address": openapi.Schema(type=openapi.TYPE_STRING),
         'content': openapi.Schema(type=openapi.TYPE_STRING),
-        'score': openapi.Schema(type=openapi.TYPE_INTEGER),
-        # 필요한 필드들을 추가로 정의합니다.
+        'category': openapi.Schema(type=openapi.TYPE_STRING),
+        'personality': openapi.Schema(type=openapi.TYPE_STRING),
+        
     }
 )
 
@@ -60,8 +64,34 @@ def get_one_worry(request):
     )
 @api_view(['POST'])
 def post_worry(request:Request):
+    # user info
+    sex = request.data["sex"]
+    age = request.data["age"]
+    job = request.data["job"]
+    address = request.data["address"]
     content = request.data['content']
-    worry = Worry.objects.create(content=content)
+    # category info
+    category = request.data['category']
+    # personality info
+    personality = request.data['personality']
+    
+    # user register
+    user_info = User(sex=sex,age=age,job=job,address=address)
+    user_info.save()
+    
+    try:
+        # category load
+        category_info = Category.objects.filter(name=category).first()
+        
+        # personality frequency + 1
+        personality_info = Personality.objects.filter(name=personality).first()
+        personality_info.frequency += 1
+        personality_info.save()
+    except Exception as e:
+        return Response(status=404, data=f"{e}")
+    
+    worry = Worry.objects.create(user=user_info,category=category_info,personality=personality_info,content=content)
+    
     return Response(status=201, data=f"{worry.id}번째 고민이 생성되었습니다.")
 
 # 고민 수정
@@ -83,7 +113,7 @@ def update_worry(request):
 
 
 @api_view(['DELETE'])
-def delete_worry(request):
+def delete_worry(request:Request):
     id = request.data['id']
     try:
         worry = Worry.objects.get(id=id)
