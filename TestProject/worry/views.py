c6e5c6265c3902e0893128561f429317c238a4b4
이원찬 <twoone14@tukorea.ac.kr>
2023-07-11 23:11:50 +0900

    fix : worry api url 수정

--- TestProject/worry/views.py
+++ TestProject/worry/views.py
@@ -79,6 +79,7 @@ worry_request_body_schema = openapi.Schema(
     }
 )
 
+
 # query string : /worry/?worry_id=1
 # -> request.GET.get("worry_id") == 1
 
@@ -86,25 +87,9 @@ worry_request_body_schema = openapi.Schema(
 # -> request["worry_id"] == 1
 
 # 모든 고민 조회
-
-
 @api_view(['GET'])
-def get_all_worry(request):
-    worries = Worry.objects.all()
-    content = []
-    for worry in worries:
-        content.append({
-            'id': worry.id,
-            'content': worry.content
-        })
-    return Response(content)
-
-# 특정 id 고민 조회
-
-
-@api_view(['GET'])
-def get_one_worry(request):
-    id = request.GET.get("worry_id")
+def get_one_worry(request, worry_id):
+    id = worry_id
     try:
         worry = Worry.objects.get(id=id)
         content = f"{id}번째 고민 : {worry.content}"
@@ -112,97 +97,109 @@ def get_one_worry(request):
     except Worry.DoesNotExist:
         return Response(status=404, data=f"{id}번째 고민이 없습니다.")
 
-# 고민 생성
+# 특정 id 고민 조회
 
 
+@swagger_auto_schema(
+    method='get',
+    operation_description="고민 전체 조회",
+)
 @swagger_auto_schema(
     method='post',
     request_body=worry_request_body_schema,
     operation_description="고민 생성",
 )
-@api_view(['POST'])
-def post_worry(request: Request):
-    # user info
-    sex = request.data["sex"]
-    age = request.data["age"]
-    job = request.data["job"]
-    nickname = request.data["nickname"]
-    address = request.data["address"]
-    if sex == None or age == None or job == None or address == None or nickname == None:
-        return Response(status=404, data=f"잘못된 입력입니다.")
-    # worry info
-    content = request.data['content']
-    if content == None or len(content) == 0:
-        return Response(status=404, data=f"잘못된 입력입니다.")
+@swagger_auto_schema(
+    method='delete',
+    operation_description="고민 post.body.id에 해당하는 고민 삭제"
+)
+@swagger_auto_schema(
+    method='put',
+    operation_description="post.body.id에 해당하는 고민 덮어쓰기 post.body.content가 필요"
+)
+@api_view(['GET', 'POST', 'DELETE', 'PUT'])
+def get_all_worry(request: Request):
+    if request.method == 'GET':
+        worries = Worry.objects.all()
+        content = []
+        for worry in worries:
+            content.append({
+                'id': worry.id,
+                'content': worry.content,
+            })
+        return Response(content)
+    elif request.method == 'POST':
+        # user info
+        sex = request.data["sex"]
+        age = request.data["age"]
+        job = request.data["job"]
+        nickname = request.data["nickname"]
+        address = request.data["address"]
+        if sex == None or age == None or job == None or address == None or nickname == None:
+            return Response(status=404, data=f"잘못된 입력입니다.")
+        # worry info
+        content = request.data['content']
+        if content == None or len(content) == 0:
+            return Response(status=404, data=f"잘못된 입력입니다.")
 
-    # category info
-    category = request.data['category']
-    if Category.objects.filter(name=category).first() is None and len(category) != 0:
-        return Response(status=404, data=f"등록된 카테고리가 없습니다.")
+        # category info
+        category = request.data['category']
+        if Category.objects.filter(name=category).first() is None and len(category) != 0:
+            return Response(status=404, data=f"등록된 카테고리가 없습니다.")
 
-    # personality info
-    personality = request.data['personality']
-    if Personality.objects.filter(name=personality).first() is None and len(personality) != 0:
-        return Response(status=404, data=f"등록된 인물이 없습니다.")
+        # personality info
+        personality = request.data['personality']
+        if Personality.objects.filter(name=personality).first() is None and len(personality) != 0:
+            return Response(status=404, data=f"등록된 인물이 없습니다.")
 
-    # user register
-    user_info = User(sex=sex, age=age, job=job,
-                     nickname=nickname, address=address)
-    user_info.save()
+        # user register
+        user_info = User(sex=sex, age=age, job=job,
+                         nickname=nickname, address=address)
+        user_info.save()
 
-    try:
-        # category load
-        category_info = Category.objects.filter(name=category).first()
+        try:
+            # category load
+            category_info = Category.objects.filter(name=category).first()
 
-        # personality frequency + 1
-        personality_info = Personality.objects.filter(name=personality).first()
-        personality_info.frequency += 1
-        personality_info.save()
-    except Exception as e:
-        return Response(status=404, data=f"{e}")
+            # personality frequency + 1
+            personality_info = Personality.objects.filter(name=personality).first()
+            personality_info.frequency += 1
+            personality_info.save()
+        except Exception as e:
+            return Response(status=404, data=f"{e}")
 
-    worry = Worry.objects.create(
-        user=user_info, category=category_info, personality=personality_info, content=content)
+        worry = Worry.objects.create(
+            user=user_info, category=category_info, personality=personality_info, content=content)
 
-    json_data = {
-        "name": nickname,
-        "sex": sex,
-        "age": age,
-        "job": job,
-        "address": address,
-        "content": content,
-        "category": category,
-        "personality": personality,
-        "worry": worry,
-    }
-    return Response(status=200, data=gpt_answer(json_data))
+        json_data = {
+            "name": nickname,
+            "sex": sex,
+            "age": age,
+            "job": job,
+            "address": address,
+            "content": content,
+            "category": category,
+            "personality": personality,
+            "worry": worry,
+        }
+        return Response(status=200, data=gpt_answer(json_data))
+    elif  request.method == 'PUT':
+        id = request.data['id']
+        content = request.data['content']
+        try:
+            worry = Worry.objects.get(id=id)
+            worry.content = content
+            worry.save()
+            return Response(status=200, data=f"{id}번째 고민이 수정되었습니다.")
+        except Worry.DoesNotExist:
+            return Response(status=404, data=f"{id}번째 고민이 없습니다.")
+    elif request.method == 'DELETE':
+        id = request.data['id']
+        try:
+            worry = Worry.objects.get(id=id)
+            worry.delete()
+            return Response(status=200, data=f"{id}번째 고민이 삭제되었습니다.")
+        except Worry.DoesNotExist:
+            return Response(status=404, data=f"{id}번째 고민이 없습니다.")
 
 
-# 고민 수정
-
-
-@api_view(['PUT'])
-def update_worry(request: Request):
-    id = request.data['id']
-    content = request.data['content']
-    try:
-        worry = Worry.objects.get(id=id)
-        worry.content = content
-        worry.save()
-        return Response(status=200, data=f"{id}번째 고민이 수정되었습니다.")
-    except Worry.DoesNotExist:
-        return Response(status=404, data=f"{id}번째 고민이 없습니다.")
-
-
-# 고민 삭제
-
-
-@api_view(['DELETE'])
-def delete_worry(request: Request):
-    id = request.data['id']
-    try:
-        worry = Worry.objects.get(id=id)
-        worry.delete()
-        return Response(status=200, data=f"{id}번째 고민이 삭제되었습니다.")
-    except Worry.DoesNotExist:
-        return Response(status=404, data=f"{id}번째 고민이 없습니다.")
