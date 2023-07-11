6a52cb93e0985925b9fc99d14bbccf45c1ab682f
heesane <tees3359@gmail.com>
2023-07-11 15:24:27 +0900

    fix : model migrations

--- TestProject/category/models.py
+++ TestProject/category/models.py
@@ -1,9 +1,8 @@
-from TestProject.TestProject.basemodel import BaseModel
+from TestProject.basemodel import BaseModel
 from django.db import models
 
 
 class Category(BaseModel):
-    id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=50)
     image_url = models.CharField(max_length=500)
 
--- TestProject/worry/models.py
+++ TestProject/worry/models.py
@@ -3,22 +3,16 @@ from TestProject.basemodel import BaseModel
 from user.models import User
 from category.models import Category
 from personality.models import Personality
-from answer.models import Answer
-
 
 
 class Worry(BaseModel):  # Worry 모델
-    # TODO: 유저 모델을 연결해야함(FK)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    # TODO: 카테고리 모델을 연결해야함(FK)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    # TODO: 인격 모델을 연결해야함(FK)
-    personality = models.ForeignKey(Personality, on_delete=models.CASCADE)
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE,null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE,null=True)
+    personality = models.ForeignKey(Personality, on_delete=models.CASCADE,null=True)
     
     content = models.CharField(max_length=300)  # 내용
     
-    # TODO: 답변 연결하기
-    answer = models.CharField(max_length=300, null=True)
     # q.answer_set.all()로 접근 가능
     def __str__(self):
         return self.content
