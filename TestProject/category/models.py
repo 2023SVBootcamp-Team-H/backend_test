0bf9984a214ebe96b2a01a1832e4495fb83ef39a
이원찬 <twoone14@tukorea.ac.kr>
2023-07-11 14:05:30 +0900

    feat : category 모델 생성

--- TestProject/category/models.py
+++ TestProject/category/models.py
@@ -1,3 +1,11 @@
+from TestProject.TestProject.basemodel import BaseModel
 from django.db import models
 
-# Create your models here.
+
+class Category(BaseModel):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=50)
+    image_url = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.name
