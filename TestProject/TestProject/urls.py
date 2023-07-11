854ff2117192a57d16fb574d6f8bcadd052ab18d
heesane <tees3359@gmail.com>
2023-07-12 00:59:14 +0900

    feat : DjangoMonitoring

--- TestProject/TestProject/urls.py
+++ TestProject/TestProject/urls.py
@@ -45,4 +45,5 @@ urlpatterns = [
          cache_timeout=0), name='schema-swagger-ui'),
     path('api/docs/', schema_view.with_ui('redoc',
          cache_timeout=0), name='schema-redoc'),
+    path('', include('django_prometheus.urls')),
 ]
