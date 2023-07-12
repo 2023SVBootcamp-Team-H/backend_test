b0d22f228506b273e55fc10d204ec502b82b342c
이원찬 <twoone14@tukorea.ac.kr>
2023-07-12 23:22:00 +0900

    fix : rank null 예외처리 추가, sorted함수 최적화

--- TestProject/rank/views.py
+++ TestProject/rank/views.py
@@ -18,6 +18,9 @@ def rank(request: Request):
         # aggregate(avg=Avg('likes'))로 likes의 평균을 반환한다. (null이면 평균계산에 포함하지 않는다.)
         # { "avg" : 평균값 }
         avg = objects_filter.aggregate(avg=Avg('likes'))['avg']
+
+        # 평균이 Null 이면 == 인격에 아무도 좋아요를 누르지 않았다면 0으로 치환!
+        avg = avg if avg is not None else 0
         item = {
             "personality_name": p.name,
             "image_url": p.image_url,
@@ -25,5 +28,5 @@ def rank(request: Request):
         }
         result.append(item)
         # avg평균값을 기준으로 내림차순 정렬
-        sorted_result = sorted(result, key=lambda x: x["avg"], reverse=True)
+    sorted_result = sorted(result, key=lambda x: x["avg"], reverse=True)
     return Response({"message": "success", "result": sorted_result})
