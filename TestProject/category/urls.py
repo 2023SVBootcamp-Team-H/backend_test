02fe40b5e005b1353231dc9868cf99f074dec335
heesane <tees3359@gmail.com>
2023-07-13 22:25:17 +0900

    chore : rename

--- TestProject/category/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.category),
-    path('create/', views.create_category),
-]
--- TestProject/personality/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.personalities),
-    path('score/', views.personalities_score),
-    path('init/', views.init_personality),
-]
