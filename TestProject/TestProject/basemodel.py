ce6adb7a1ca946151744f673a4ac09a40df07bb6
heesane <tees3359@gmail.com>
2023-07-13 22:21:53 +0900

    chore : rename

--- TestProject/TestProject/basemodel.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class BaseModel(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    deleted_at = models.DateTimeField(blank=True,null=True)
-    
-    class Meta:
-        abstract = True
--- TestProject/personality/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PersonalityConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'personality'
